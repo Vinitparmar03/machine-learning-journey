ce25565427a42eb0d7679cb47aa48ada404ba73d
Vinitparmar03 <vinitparmar03@gmail.com>
2025-05-27 22:35:48 +0530

    changes made to knn.py

--- 03_classification/02_K_NN/K_NN.py
+++ 03_classification/02_K_NN/K_NN.py
@@ -65,23 +65,23 @@ plt.legend()
 plt.show()
 
 # Visualising the Test set results
-# X_set, y_set = sc.inverse_transform(X_test), y_test
-# X1, X2 = np.meshgrid(
-#     np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.5),
-#     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.5)
-# )
-# Z = classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape)
-# plt.contourf(X1, X2, Z, alpha=0.75, cmap = ListedColormap(['#FA8072', '#1E90FF']) )
-# plt.xlim(X1.min(), X1.max())
-# plt.ylim(X2.min(), X2.max())
-# colors = ['#FA8072', '#1E90FF']
-# for i, j in enumerate(np.unique(y_set)):
-#     plt.scatter(
-#         X_set[y_set == j, 0], X_set[y_set == j, 1],
-#         color=colors[i], label=j
-#     )
-# plt.title('K-NN (Test set)')
-# plt.xlabel('Age')
-# plt.ylabel('Estimated Salary')
-# plt.legend()
-# plt.show()
+X_set, y_set = sc.inverse_transform(X_test), y_test
+X1, X2 = np.meshgrid(
+    np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.5),
+    np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.5)
+)
+Z = classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape)
+plt.contourf(X1, X2, Z, alpha=0.75, cmap = ListedColormap(['#FA8072', '#1E90FF']) )
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+colors = ['#FA8072', '#1E90FF']
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(
+        X_set[y_set == j, 0], X_set[y_set == j, 1],
+        color=colors[i], label=j
+    )
+plt.title('K-NN (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
